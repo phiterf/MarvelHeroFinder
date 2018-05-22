ff49bdf063bd2c687c2f30845a476babf9e81309
Phiter Fernandes <phiterfernandes@gmail.com>
2018-05-22 07:24:14 -0300

    Atualizacão dos pacotes bootstrap e jQuery. Inclusão do Vue e Axios. Atualizacão da busca de personagem e rotas.

--- TesteAcerto/App_Start/WebApiConfig.cs
+++ TesteAcerto/App_Start/WebApiConfig.cs
@@ -14,6 +14,8 @@ namespace TesteAcerto
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.MediaTypeMappings.Add(new System.Net.Http.Formatting.RequestHeaderMapping("Accept", "text/html", StringComparison.InvariantCultureIgnoreCase, true, "application/json"));
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
